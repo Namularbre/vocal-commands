d194b39bf824e5bcef8d24c24ae0aeef573eca6b
Thomas SAZERAT <namulabre@gmail.com>
2023-12-06 01:55:29 +0100

    main: fixed a bug where a .dotenv key was not converted to int

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if load_dotenv():
         try:
             if getenv("MICROPHONE_INDEX"):
-                microphone_index = getenv("MICROPHONE_INDEX")
+                microphone_index = int(getenv("MICROPHONE_INDEX"))
             else:
                 microphone_index = 0
             voice_listener.capture_voice_input(microphone_index=microphone_index)
