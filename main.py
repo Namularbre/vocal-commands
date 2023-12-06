e0bb7a25b52bfe860adf7fca5070c448982943bb
Thomas SAZERAT <namulabre@gmail.com>
2023-12-06 01:38:00 +0100

    main: Added the .env microphone selection

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import voice_listener
 from dotenv import load_dotenv
+from os import getenv
 
 
 if __name__ == "__main__":
     if load_dotenv():
         try:
-            voice_listener.capture_voice_input()
+            if getenv("MICROPHONE_INDEX"):
+                microphone_index = getenv("MICROPHONE_INDEX")
+            else:
+                microphone_index = 0
+            voice_listener.capture_voice_input(microphone_index=microphone_index)
         except KeyboardInterrupt:
             exit(0)
     else:
