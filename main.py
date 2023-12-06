d8192b7bd6cbc7b8d902407e96b12944349d41b3
Thomas SAZERAT <namulabre@gmail.com>
2023-12-06 01:09:32 +0100

    main.py added an error handling for missing .env

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import voice_listener
 from dotenv import load_dotenv
 
+
 if __name__ == "__main__":
-    load_dotenv()
-    try:
-        voice_listener.capture_voice_input()
-    except KeyboardInterrupt:
-        exit(0)
+    if load_dotenv():
+        try:
+            voice_listener.capture_voice_input()
+        except KeyboardInterrupt:
+            exit(0)
+    else:
+        print("Error: .env file is not found")
