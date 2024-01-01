b46b1a31a093035f90333cf1cca7b4e3bcbaf43c
Thomas SAZERAT <namulabre@gmail.com>
2024-01-01 20:35:04 +0100

    renamed firefox_path to web_browser_path

--- web_searcher/__init__.py
+++ web_searcher/__init__.py
@@ -3,14 +3,14 @@ from os import getenv
 
 
 def search_on_browser(query: str) -> None:
-    webbrowser_path = getenv("WEB_BROWSER_PATH")
-    print(webbrowser_path)
+    web_browser_path = getenv("WEB_BROWSER_PATH")
+    print(web_browser_path)
     search_url = "https://www.google.com/search?q=" + query.strip()
     try:
-        if webbrowser_path:
-            webbrowser.register('web_browser', None, webbrowser.BackgroundBrowser(webbrowser_path))
+        if web_browser_path:
+            webbrowser.register('web_browser', None, webbrowser.BackgroundBrowser(web_browser_path))
             webbrowser.get('web_browser').open_new_tab(search_url)
         else:
             webbrowser.open(search_url)
     except FileNotFoundError as e:
-        print(f"Unable to open web browser with path {webbrowser_path} : {e}")
+        print(f"Unable to open web browser with path {web_browser_path} : {e}")
